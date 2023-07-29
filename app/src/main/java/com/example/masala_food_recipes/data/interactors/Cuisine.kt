1054e1602f420026de5a8cdd3c744953a8bb5020
menna <94606987+Menna120@users.noreply.github.com>
2023-07-29 18:06:58 +0300

    rewrite cuisine class

--- app/src/main/java/com/example/masala_food_recipes/data/interactors/Cuisine.kt
+++ app/src/main/java/com/example/masala_food_recipes/data/interactors/Cuisine.kt
@@ -4,20 +4,7 @@ import com.example.masala_food_recipes.data.entities.Recipe
 
 class Cuisine(private val recipes:List<Recipe>) {
 
-    fun getCuisines():Map<String,Int>{
-        val mapCuisine = mutableMapOf<String,Int>()
-        recipes.forEach {
-            mapCuisine.putIfAbsent(it.cuisine,0)
-            mapCuisine[it.cuisine] = mapCuisine[it.cuisine]!! + 1
-        }
-        return mapCuisine
-    }
+    val cuisineRecipes = recipes.groupBy({ it.cuisine }, { it.translatedRecipeName })
 
-    fun getRecipesName():Map<String,List<String>>{
-        val mapRecipe= mutableMapOf<String,List<String>>()
-        recipes.shuffled().forEach {
-            mapRecipe[it.translatedRecipeName]?.plus(it.cuisine)
-        }
-        return mapRecipe
-    }
+    fun getCuisineRecipesNumber(): Map<String, Int> = cuisineRecipes.mapValues { it.value.size }
 }
