bb7e60e375fd93de6d9ea7976812dd796d159e99
Ali <alialashwall14@gmail.com>
2023-07-29 16:50:56 +0300

    Interactor

---
+++ app/src/main/java/com/example/masala_food_recipes/Interactor.kt
@@ -0,0 +1,29 @@
+package com.example.masala_food_recipes
+
+import com.example.masala_food_recipes.data.DataManager
+import com.example.masala_food_recipes.data.entities.Recipe
+
+class Interactor(context: MainActivity) {
+
+    private val recipes = DataManager(context).getAllRecipesData()
+
+    fun underTwentyMin(): List<Recipe>{
+        val list = mutableListOf<Recipe>()
+        recipes.forEach{
+            if (it.totalTimeInMins.toInt() < 20){
+                list.add(it)
+            }
+        }
+        return list.shuffled()
+    }
+
+    fun underFiveIngredient(): List<Recipe>{
+        val list =  mutableListOf<Recipe>()
+        recipes.forEach {
+            if (it.ingredientCount.toInt() < 5){
+                list.add(it)
+            }
+        }
+        return list.shuffled()
+    }
+}
