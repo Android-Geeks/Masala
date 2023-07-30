f219b9cb7a68b9d91f7f09bcfaabd70401aa720b
Zeyad Abdullah <zeyadgalal93@gmail.com>
2023-07-30 17:51:05 +0300

    refactor design and remove conflicts

--- app/src/main/java/com/example/masala_food_recipes/MainActivity.kt
+++ app/src/main/java/com/example/masala_food_recipes/MainActivity.kt
@@ -12,17 +12,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        // Cuisine Card Info
-        Cuisines(DataManager(this).getAllRecipesData()).getCuisineCards().forEach {
-            Log.d("Cuisine", "Cuisine: ${it.key} has ${it.value.first} recipes and the image url is ${it.value.second}")
-        }
-
-        // For You List
-        ForYouRecipe(DataManager(this).getAllRecipesData()).execute(20).forEach {
-            Log.d("For You", "Recipe Name is ${it.key} and the cuisine is ${it.value.first} and the image url is ${it.value.second}")
-        }
-
     }
 }
 
