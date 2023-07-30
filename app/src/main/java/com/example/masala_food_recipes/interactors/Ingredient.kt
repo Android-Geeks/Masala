0a7c83b1f38f3f4d26b67d7b61e4f1d3dfb68866
Zeyad Abdullah <zeyadgalal93@gmail.com>
2023-07-30 06:04:25 +0300

    refactor Ingredient, RecipeTime

--- app/src/main/java/com/example/masala_food_recipes/interactors/Ingredient.kt
+++ app/src/main/java/com/example/masala_food_recipes/interactors/Ingredient.kt
@@ -2,15 +2,11 @@ package com.example.masala_food_recipes.interactors
 
 import com.example.masala_food_recipes.data.entities.Recipe
 
-class Ingredient (private val recipes: List<Recipe>){
-
-    fun underFiveIngredient(): List<Recipe>{
-        val list =  mutableListOf<Recipe>()
-        recipes.forEach {
-            if (it.ingredientCount.toInt() < 5){
-                list.add(it)
-            }
-        }
-        return list.shuffled()
-    }
+class Ingredient(private val recipes: List<Recipe>) {
+    fun underFiveIngredient(limit: Int) =
+        recipes
+            .filter { it.ingredientCount.toInt() < 5 }
+            .take(limit)
+            .shuffled()
+            .associate { (Pair(it.translatedRecipeName, Pair(it.totalTimeInMins, it.imageUrl))) }
 }
--- app/src/main/java/com/example/masala_food_recipes/interactors/RecipeTime.kt
+++ app/src/main/java/com/example/masala_food_recipes/interactors/RecipeTime.kt
@@ -2,15 +2,12 @@ package com.example.masala_food_recipes.interactors
 
 import com.example.masala_food_recipes.data.entities.Recipe
 
-class RecipeTime (private val recipes : List<Recipe> ) {
+class RecipeTime(private val recipes: List<Recipe>) {
 
-    fun underTwentyMin(): List<Recipe>{
-        val list = mutableListOf<Recipe>()
-        recipes.forEach{
-            if (it.totalTimeInMins.toInt() < 20){
-                list.add(it)
-            }
-        }
-        return list.shuffled()
-    }
+    fun underTwentyMin(limit: Int) =
+        recipes
+            .filter { it.totalTimeInMins.toInt() < 20 }
+            .take(limit)
+            .shuffled()
+            .associate { (Pair(it.translatedRecipeName, Pair(it.totalTimeInMins, it.imageUrl))) }
 }
