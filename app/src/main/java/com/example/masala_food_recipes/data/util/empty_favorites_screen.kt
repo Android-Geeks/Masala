a72d62c28d1c5fe69539955954148c72a05fec93
TRIO STORE <nour@google.com>
2023-07-31 22:33:22 +0300

    styles

---
+++ app/src/main/java/com/example/masala_food_recipes/data/util/empty_favorites_screen.kt
@@ -0,0 +1,17 @@
+package com.example.masala_food_recipes.data.util
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.masala_food_recipes.R
+
+class empty_favorites_screen:Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.empty_favorites_screen, container, false)
+    }
+}
