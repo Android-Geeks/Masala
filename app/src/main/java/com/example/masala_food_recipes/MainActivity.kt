0a96c3f866f3b4020b44712052848d944a003879
Zeyad Abdullah <zeyadgalal93@gmail.com>
2023-08-12 14:23:53 +0300

    remove unwanted abstract activity

--- app/src/main/java/com/example/masala_food_recipes/MainActivity.kt
+++ app/src/main/java/com/example/masala_food_recipes/MainActivity.kt
@@ -2,12 +2,56 @@ package com.example.masala_food_recipes
 
 
 import android.os.Bundle
-import com.example.masala_food_recipes.ui.InitMainActivity
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import com.example.masala_food_recipes.databinding.ActivityMainBinding
+import com.example.masala_food_recipes.ui.fragment.FavouriteFragment
+import com.example.masala_food_recipes.ui.fragment.HomeFragment
+import com.example.masala_food_recipes.ui.fragment.SearchFragment
+import com.example.masala_food_recipes.ui.fragment.SettingFragment
 
-class MainActivity : InitMainActivity() {
+class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState : Bundle?) {
+    private lateinit var binding: ActivityMainBinding
+    private val homeScreen = HomeFragment()
+    private val favouriteScreen = FavouriteFragment()
+    private val searchScreen = SearchFragment()
+    private val settingScreen = SettingFragment()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        init()
+    }
+
+    private fun init() {
+        initFragment()
+        binding.bottomNavigation.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.home_icon -> replaceFragment(homeScreen)
+                R.id.search_icon -> replaceFragment(searchScreen)
+                R.id.favourite_icon -> replaceFragment(favouriteScreen)
+                R.id.setting_icon -> replaceFragment(settingScreen)
+                else -> false
+            }
+        }
+    }
+
+    private fun initFragment() {
+        inTransaction { add(R.id.fragment_container_view, homeScreen) }
+    }
+
+    private fun replaceFragment(fragment: Fragment): Boolean {
+        inTransaction { replace(R.id.fragment_container_view, fragment) }
+        return true
+    }
+
+    private fun inTransaction(func: FragmentTransaction.() -> FragmentTransaction) {
+        supportFragmentManager
+            .beginTransaction()
+            .func()
+            .commit()
     }
 
 }
--- app/src/main/java/com/example/masala_food_recipes/ui/fragment/BaseFragment.kt
+++ app/src/main/java/com/example/masala_food_recipes/ui/fragment/BaseFragment.kt
@@ -8,17 +8,17 @@ import androidx.fragment.app.Fragment
 import androidx.viewbinding.ViewBinding
 
 abstract class BaseFragment<VB : ViewBinding>(
-        private val bindingInflater : (inflater : LayoutInflater) -> VB
+    private val bindingInflater: (inflater: LayoutInflater) -> VB
 ) : Fragment() {
-    private lateinit var _binding : VB
-    protected val binding : VB
+    private lateinit var _binding: VB
+    protected val binding: VB
         get() = _binding
 
     override fun onCreateView(
-            inflater : LayoutInflater ,
-            container : ViewGroup? ,
-            savedInstanceState : Bundle?
-    ) : View? {
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         _binding = bindingInflater(inflater)
         onCreateView()
         return binding.root
