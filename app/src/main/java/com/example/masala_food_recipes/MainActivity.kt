1a93fe8fffff707cb78fbfd186a56eba4e9a49e1
Ali <alialashwall14@gmail.com>
2023-08-13 08:21:00 +0300

    .

--- app/src/main/java/com/example/masala_food_recipes/MainActivity.kt
+++ app/src/main/java/com/example/masala_food_recipes/MainActivity.kt
@@ -1,36 +1,57 @@
 package com.example.masala_food_recipes
 
-import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import com.example.masala_food_recipes.data.DataManager
-import com.example.masala_food_recipes.databinding.ActivityMainBinding
-import com.example.masala_food_recipes.ui.recyclerview.CuisineAdapter
-import com.example.masala_food_recipes.ui.recyclerview.ForYouRecipeAdapter
-import com.example.masala_food_recipes.ui.recyclerview.UnderFiveIngredientAdapter
-import com.example.masala_food_recipes.ui.recyclerview.UnderTwentyMinAdapter
 
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import com.example.masala_food_recipes.databinding.ActivityMainBinding
+import com.example.masala_food_recipes.ui.fragment.FavouriteFragment
+import com.example.masala_food_recipes.ui.fragment.HomeFragment
+import com.example.masala_food_recipes.ui.fragment.SearchFragment
+import com.example.masala_food_recipes.ui.fragment.SettingFragment
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
-    @SuppressLint("SetTextI18n")
+
+    private lateinit var binding: ActivityMainBinding
+    private val homeScreen = HomeFragment()
+    private val favouriteScreen = FavouriteFragment()
+    private val searchScreen = SearchFragment()
+    private val settingScreen = SettingFragment()
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
         setContentView(binding.root)
-
-        val recipeList = DataManager(this).getAllRecipesData()
-
-        binding.cuisineRecycler.adapter = CuisineAdapter(recipeList)
-
-        binding.recipeRecycler.adapter = ForYouRecipeAdapter(recipeList)
-
-        binding.underFiveRecycler.adapter = UnderFiveIngredientAdapter(recipeList)
-        binding.underTwentyRecycler.adapter = UnderTwentyMinAdapter(recipeList)
-
-
-
-
-
+        init()
     }
+
+    private fun init() {
+        initFragment()
+        binding.bottomNavigation.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.home_icon -> replaceFragment(homeScreen)
+                R.id.search_icon -> replaceFragment(searchScreen)
+                R.id.favourite_icon -> replaceFragment(favouriteScreen)
+                R.id.setting_icon -> replaceFragment(settingScreen)
+                else -> false
+            }
+        }
+    }
+
+    private fun initFragment() {
+        inTransaction { add(R.id.fragment_container_view, homeScreen) }
+    }
+
+    private fun replaceFragment(fragment: Fragment): Boolean {
+        inTransaction { replace(R.id.fragment_container_view, fragment) }
+        return true
+    }
+
+    private fun inTransaction(func: FragmentTransaction.() -> FragmentTransaction) {
+        supportFragmentManager
+            .beginTransaction()
+            .func()
+            .commit()
+    }
+
 }
