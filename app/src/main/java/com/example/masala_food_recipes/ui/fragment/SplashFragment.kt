01f8d56c5ed10d7b7580c923f842cf4fda6fc3c7
TRIO STORE <nour@google.com>
2023-08-15 19:41:32 +0300

    Done

---
+++ app/src/main/java/com/example/masala_food_recipes/ui/fragment/SplashFragment.kt
@@ -0,0 +1,11 @@
+package com.example.masala_food_recipes.ui.fragment
+
+import com.example.masala_food_recipes.databinding.FragmentFavouriteBinding
+
+class SplashFragment : BaseFragment<FragmentFavouriteBinding>(FragmentFavouriteBinding :: inflate) {
+
+    override fun onCreateView() {
+        //TODO("Not yet implemented")
+    }
+
+}
