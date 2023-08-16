ab0dc3618c6c4fb326e58c590df6a74d2b6e0310
Zeyad Abdullah <zeyadgalal93@gmail.com>
2023-08-16 10:11:36 +0300

    fix spacing between recycler items

--- app/src/main/java/com/example/masala_food_recipes/ui/fragment/UnderFiveIngredientFragment.kt
+++ app/src/main/java/com/example/masala_food_recipes/ui/fragment/UnderFiveIngredientFragment.kt
@@ -1,5 +1,6 @@
 package com.example.masala_food_recipes.ui.fragment
 
+import AdaptiveSpacingItemDecoration
 import com.example.masala_food_recipes.data.DataManager
 import com.example.masala_food_recipes.data.interactors.UnderFiveIngredient
 import com.example.masala_food_recipes.databinding.SubCategoryRecyclerBinding
@@ -9,7 +10,8 @@ class UnderFiveIngredientFragment :
     BaseFragment<SubCategoryRecyclerBinding>(SubCategoryRecyclerBinding::inflate) {
     override fun onCreateView() {
         val under5Ingredient =
-                UnderFiveIngredient(DataManager(this.requireContext()).getAllRecipesData()).execute()
-        binding.subCategory.adapter = SubCategoryAdapter(under5Ingredient , null)
+            UnderFiveIngredient(DataManager(this.requireContext()).getAllRecipesData()).execute()
+        binding.subCategory.addItemDecoration(AdaptiveSpacingItemDecoration(32, false))
+        binding.subCategory.adapter = SubCategoryAdapter(under5Ingredient, null)
     }
 }
