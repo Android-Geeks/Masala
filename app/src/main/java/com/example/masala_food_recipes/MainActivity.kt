85387626edeecc40e0b28caa4a07225c6a6fd76f
menna <94606987+Menna120@users.noreply.github.com>
2023-08-19 13:30:44 +0300

    add setting and favourite app bar

--- app/src/main/java/com/example/masala_food_recipes/MainActivity.kt
+++ app/src/main/java/com/example/masala_food_recipes/MainActivity.kt
@@ -8,63 +8,76 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
 import com.example.masala_food_recipes.databinding.ActivityMainBinding
 import com.example.masala_food_recipes.ui.fragment.FavouriteFragment
+import com.example.masala_food_recipes.ui.fragment.HomeFragment
 import com.example.masala_food_recipes.ui.fragment.SearchFragment
 import com.example.masala_food_recipes.ui.fragment.SettingFragment
-import com.example.masala_food_recipes.ui.fragment.UnderTwentyMinFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity()
+{
 
-    private lateinit var binding: ActivityMainBinding
-    private val homeScreen = UnderTwentyMinFragment()
+    private lateinit var binding : ActivityMainBinding
+    private val homeScreen = HomeFragment()
     private val favouriteScreen = FavouriteFragment()
     private val searchScreen = SearchFragment()
     private val settingScreen = SettingFragment()
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState : Bundle?)
+    {
         binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        changeAppBar(R.layout.main_app_bar)
         init(savedInstanceState)
     }
 
-    private fun init(savedInstanceState:Bundle?) {
-        if (savedInstanceState == null) {
+    private fun init(savedInstanceState : Bundle?)
+    {
+        if (savedInstanceState == null)
+        {
             initFragment()
+            changeAppBar(R.layout.main_app_bar)
         }
         binding.bottomNavigation.setOnItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.home_icon -> replaceFragment(homeScreen)
-                R.id.search_icon -> replaceFragment(searchScreen)
-                R.id.favourite_icon -> replaceFragment(favouriteScreen)
-                R.id.setting_icon -> replaceFragment(settingScreen)
-                else -> false
+            when (item.itemId)
+            {
+                R.id.home_icon      -> replaceFragment(homeScreen , R.layout.main_app_bar)
+                R.id.search_icon    -> replaceFragment(searchScreen , R.layout.main_app_bar)
+                R.id.favourite_icon -> replaceFragment(favouriteScreen , R.layout.favourite_app_bar)
+                R.id.setting_icon   -> replaceFragment(settingScreen , null)
+                else                -> false
             }
         }
     }
 
-    private fun initFragment() {
-        inTransaction { replace(R.id.fragment_container_view, homeScreen) }
+    private fun initFragment()
+    {
+        inTransaction { replace(R.id.fragment_container_view , homeScreen) }
     }
 
-    private fun replaceFragment(fragment: Fragment): Boolean {
-        inTransaction { replace(R.id.fragment_container_view, fragment) }
+    private fun replaceFragment(fragment : Fragment , appBar : Int?) : Boolean
+    {
+        inTransaction { replace(R.id.fragment_container_view , fragment) }
+        changeAppBar(appBar)
         return true
     }
 
 
-    private fun inTransaction(func: FragmentTransaction.() -> FragmentTransaction) {
+    private fun inTransaction(func : FragmentTransaction.() -> FragmentTransaction)
+    {
         supportFragmentManager
-            .beginTransaction()
-            .func()
-            .commit()
+                .beginTransaction()
+                .func()
+                .commit()
     }
 
-    private fun changeAppBar(appBar : Int) {
+    private fun changeAppBar(appBar : Int?)
+    {
         val appBarContainer = binding.appBar
         appBarContainer.removeAllViews()
-        val inflater = LayoutInflater.from(this)
-        val newAppBar = inflater.inflate(appBar , appBarContainer , false)
-        appBarContainer.addView(newAppBar)
+        if (appBar != null)
+        {
+            val inflater = LayoutInflater.from(this)
+            val newAppBar = inflater.inflate(appBar , appBarContainer , false)
+            appBarContainer.addView(newAppBar)
+        }
 
     }
 
