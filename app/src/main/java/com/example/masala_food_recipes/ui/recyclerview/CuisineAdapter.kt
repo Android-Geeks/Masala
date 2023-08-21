311fd6777b06fc00b53f397ebb35377a975c76ea
Ali <alialashwall14@gmail.com>
2023-08-21 09:02:09 +0300

    edit

--- app/src/main/java/com/example/masala_food_recipes/ui/recyclerview/CuisineAdapter.kt
+++ app/src/main/java/com/example/masala_food_recipes/ui/recyclerview/CuisineAdapter.kt
@@ -5,16 +5,9 @@ import android.content.Context
 import android.view.View
 import com.bumptech.glide.Glide
 import com.example.masala_food_recipes.R
-import com.example.masala_food_recipes.data.DataManager
-import com.example.masala_food_recipes.data.entities.Recipe
-import com.example.masala_food_recipes.data.interactors.Cuisines
 import com.example.masala_food_recipes.databinding.CardViewCuisinesBinding
 
-interface CuisineListener :BaseInteractionListener{
-//    fun doNothing() {
-//        // Empty implementation
-//    }
-}
+interface CuisineListener :BaseInteractionListener
 
 class CuisineAdapter(items: List<List<String>>, listener: CuisineListener) :BaseRecyclerAdapter<List<String>,BaseRecyclerAdapter.BaseViewHolder<List<String>>>(items, listener) {
     override val layoutId = R.layout.card_view_cuisines
@@ -24,12 +17,8 @@ class CuisineAdapter(items: List<List<String>>, listener: CuisineListener) :Base
     class CuisineViewHolder(itemView: View) : BaseViewHolder<List<String>>(itemView) {
         private val binding = CardViewCuisinesBinding.bind(itemView)
         private val context: Context = itemView.context
-        private val allCuisines = Cuisines(DataManager(context).getAllRecipesData()).getCuisineCards()
-        //item consist of only one recipe(Cuisine) not list of recipes so we need to get all cuisines to count The number of repetitions
         @SuppressLint("SetTextI18n")
         override fun bind(item: List<String>) {
-//            val cuisineMap = Cuisines(listOf(item)).getCuisineCards()
-//            val key = cuisineMap.keys.joinToString(",")
             binding.apply {
                 cuisineTextview.text = item[0]
                 itemsCount.text = "${item[1]} item"
