b3f167d9070559a45a851f533c20a647338575bf
Zeyad Abdullah <zeyadgalal93@gmail.com>
2023-08-22 15:20:46 +0300

    add parcel implementation

--- app/src/main/java/com/example/masala_food_recipes/data/entities/Recipe.kt
+++ app/src/main/java/com/example/masala_food_recipes/data/entities/Recipe.kt
@@ -1,8 +1,9 @@
 package com.example.masala_food_recipes.data.entities
 
-import android.os.Parcel
 import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class Recipe(
         val translatedRecipeName : String ,
         val translatedIngredients : List<String> ,
@@ -14,38 +15,3 @@ data class Recipe(
         val imageUrl : String ,
         val ingredientCount : String ,
 ) : Parcelable
-{
-    constructor(parcel : Parcel) : this(
-            parcel.readString()!! ,
-            parcel.createStringArrayList()!! ,
-            parcel.readString()!! ,
-            parcel.readString()!! ,
-            parcel.createStringArrayList()!! ,
-            parcel.readString()!! ,
-            parcel.createStringArrayList()!! ,
-            parcel.readString()!! ,
-            parcel.readString()!!
-    )
-
-    override fun writeToParcel(parcel : Parcel , flags : Int)
-    {
-        parcel.writeString(translatedRecipeName)
-        parcel.writeStringList(translatedIngredients)
-        parcel.writeString(totalTimeInMin)
-        parcel.writeString(cuisine)
-        parcel.writeStringList(translatedInstructions)
-        parcel.writeString(url)
-        parcel.writeStringList(cleanedIngredients)
-        parcel.writeString(imageUrl)
-        parcel.writeString(ingredientCount)
-    }
-
-    override fun describeContents() = 0
-
-    companion object CREATOR : Parcelable.Creator<Recipe>
-    {
-        override fun createFromParcel(parcel : Parcel) = Recipe(parcel)
-
-        override fun newArray(size : Int) : Array<Recipe?> = arrayOfNulls(size)
-    }
-}
