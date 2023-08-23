3517d757b584994376895ec077fe448c2a8acc96
Ali <alialashwall14@gmail.com>
2023-08-24 02:29:33 +0300

    .

--- app/src/main/java/com/example/masala_food_recipes/ui/fragment/SearchFragment.kt
+++ app/src/main/java/com/example/masala_food_recipes/ui/fragment/SearchFragment.kt
@@ -4,7 +4,6 @@ import android.view.View
 import com.example.masala_food_recipes.data.DataManager
 import com.example.masala_food_recipes.data.interactors.SearchRecipe
 import com.example.masala_food_recipes.databinding.FragmentSearchBinding
-import com.example.masala_food_recipes.ui.recyclerview.ItemsPaddingDecoration
 import com.example.masala_food_recipes.ui.recyclerview.SearchAdapter
 import com.example.masala_food_recipes.ui.recyclerview.SearchListener
 
@@ -14,7 +13,7 @@ class SearchFragment : BaseFragment<FragmentSearchBinding>(FragmentSearchBinding
         val recipeList = DataManager(requireContext()).getAllRecipesData()
         val searchList = SearchRecipe(recipeList).execute()
 
-        binding.buttonSearch.setOnClickListener {
+        binding.buttonSearchIcon.setOnClickListener {
             val name = binding.searchBar.text.toString()
             val newList = searchList.filter {
                 it[0].substring(0, name.length).equals(name, ignoreCase = true)
@@ -36,7 +35,7 @@ class SearchFragment : BaseFragment<FragmentSearchBinding>(FragmentSearchBinding
             binding.apply {
                 emptySearch.root.visibility = View.GONE
                 searchedRecycler.visibility = View.VISIBLE
-                searchedRecycler.addItemDecoration(ItemsPaddingDecoration(16,newList.size))
+//                searchedRecycler.addItemDecoration(ItemsPaddingDecoration(16,newList.size-1))
                 searchedRecycler.adapter = SearchAdapter(newList, object : SearchListener {})
             }
         }
