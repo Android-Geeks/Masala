1d76afd556e7d396ad3330a2ee916cf570b7c7aa
Ali <alialashwall14@gmail.com>
2023-08-24 13:04:51 +0300

    Search recycler

--- app/src/main/java/com/example/masala_food_recipes/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/masala_food_recipes/ui/fragment/HomeFragment.kt
@@ -1,7 +1,6 @@
 package com.example.masala_food_recipes.ui.fragment
 
-import androidx.navigation.Navigation
-import com.example.masala_food_recipes.R
+
 import com.example.masala_food_recipes.data.entities.ChildItem
 import com.example.masala_food_recipes.databinding.FragmentHomeBinding
 import com.example.masala_food_recipes.ui.recyclerview.ChildAdapter
@@ -9,12 +8,11 @@ import com.example.masala_food_recipes.ui.recyclerview.ChildAdapter
 class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding :: inflate) {
 
     override fun onCreateView() {
-        val cuisineFragmentID = R.id.action_homeFragment_to_cuisineScreenFragment
-        val subCategory = R.id.action_homeFragment_to_favouriteFragment2       // until creating sub category fragment
-        val child1 = ChildItem("Cuisine", "view all", "CuisineAdapter", cuisineFragmentID)
-        val child2 = ChildItem("For You", "view all", "ForYouRecipeAdapter", subCategory)
-        val child3 = ChildItem("under 20 min meal", "view all", "UnderTwentyMinAdapter", subCategory)
-        val child4 = ChildItem("under 5 ingredient meal", "view all", "UnderFiveIngredientAdapter", subCategory)
+
+        val child1 = ChildItem("Cuisine", "view all", "CuisineAdapter")
+        val child2 = ChildItem("For You", "view all", "ForYouRecipeAdapter")
+        val child3 = ChildItem("under 20 min meal", "view all", "UnderTwentyMinAdapter")
+        val child4 = ChildItem("under 5 ingredient meal", "view all", "UnderFiveIngredientAdapter")
         val childList: List<ChildItem> = listOf(child1, child2, child3, child4)
 
         
--- app/src/main/java/com/example/masala_food_recipes/ui/recyclerview/ChildAdapter.kt
+++ app/src/main/java/com/example/masala_food_recipes/ui/recyclerview/ChildAdapter.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import com.example.masala_food_recipes.data.entities.ChildItem
 import com.example.masala_food_recipes.R
@@ -39,9 +38,6 @@ class ChildAdapter(
             cuisineText.text = childItem.type
             viewAllText.text = childItem.view_all
             childRecycler.adapter = getAdapter(childItem.adapterType)
-            viewAllText.setOnClickListener { v->
-                Navigation.findNavController(v).navigate(childItem.navId)
-            }
         }
     }
 
