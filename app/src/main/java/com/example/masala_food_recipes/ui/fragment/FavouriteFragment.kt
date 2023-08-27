8e2fc26f4a29abd56e35b0e09f2a0afb9e0cff4f
Zeinab979 <zeinabwahdan3677@gmail.com>
2023-08-28 00:50:52 +0300

    edit favourite screen

--- app/src/main/java/com/example/masala_food_recipes/ui/fragment/FavouriteFragment.kt
+++ app/src/main/java/com/example/masala_food_recipes/ui/fragment/FavouriteFragment.kt
@@ -1,11 +1,42 @@
 package com.example.masala_food_recipes.ui.fragment
 
+import android.content.Context
 import com.example.masala_food_recipes.databinding.FragmentFavouriteBinding
+import com.example.masala_food_recipes.ui.recyclerview.FavouriteScreenAdapter
+
 
 class FavouriteFragment :
     BaseFragment<FragmentFavouriteBinding>(FragmentFavouriteBinding::inflate) {
+    private val adapter by lazy { FavouriteScreenAdapter() }
     override fun onCreateView() {
-        //TODO("Not yet implemented")
+        val sharedPref = requireContext().getSharedPreferences("Favourites", Context.MODE_PRIVATE)
+
+        val favList = mutableListOf<List<String>>(
+
+            listOf(
+                "masalsa",
+                "20",
+                "https://dfstudio-d420.kxcdn.com/wordpress/wp-content/uploads/2019/06/digital_camera_photo-980x653.jpg"
+            ),
+            listOf(
+                "masalsa",
+                "20",
+                "https://dfstudio-d420.kxcdn.com/wordpress/wp-content/uploads/2019/06/digital_camera_photo-980x653.jpg"
+            ),
+            listOf(
+                "masalsa",
+                "20",
+                "https://dfstudio-d420.kxcdn.com/wordpress/wp-content/uploads/2019/06/digital_camera_photo-980x653.jpg"
+            )
+        )
+        //val favList by lazy { sharedPref.getStringSet("Favourite", emptySet())?.toList() ?: emptyList() }
+
+        adapter.differ.submitList(favList)
+
+
+        binding.recyclerView.adapter = adapter
+
+
     }
 
 }
---
+++ app/src/main/java/com/example/masala_food_recipes/ui/recyclerview/FavouriteScreenAdapter.kt
@@ -0,0 +1,79 @@
+package com.example.masala_food_recipes.ui.recyclerview
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.masala_food_recipes.R
+import com.example.masala_food_recipes.databinding.CardViewFavouriteBinding
+
+class FavouriteScreenAdapter: RecyclerView.Adapter<FavouriteScreenAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.card_view_favourite, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(differ.currentList[position])
+        holder.setIsRecyclable(false)
+
+    }
+
+    override fun getItemCount(): Int {
+        return differ.currentList.size
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val binding = CardViewFavouriteBinding.bind(itemView)
+        private val context: Context = itemView.context
+
+        fun bind(item: List<String>) {
+            binding.apply {
+                recipeText.text = item[0]
+                minutesText.text = item[1].plus(" Min")
+                Glide.with(context)
+                    .load(item[2])
+                    .placeholder(R.drawable.loading)
+                    .centerCrop()
+                    .into(binding.itemImage)
+                drawableFavorite.setOnClickListener {
+                    val position = absoluteAdapterPosition
+                    if (position != RecyclerView.NO_POSITION) {
+                        val itemToDelete = differ.currentList[position]
+                        removeItem(itemToDelete)
+                    }
+
+                }
+
+            }
+        }
+    }
+
+    private val differCallback = object : DiffUtil.ItemCallback<List<String>>() {
+        override fun areItemsTheSame(oldItem: List<String>, newItem: List<String>): Boolean {
+            return oldItem == newItem
+        }
+
+        @SuppressLint("DiffUtilEquals")
+        override fun areContentsTheSame(oldItem: List<String>, newItem: List<String>): Boolean {
+            return false
+        }
+
+    }
+
+    val differ = AsyncListDiffer(this, differCallback)
+    private fun removeItem(item: List<String>) {
+        val newList = ArrayList(differ.currentList)
+        newList.remove(item)
+        differ.submitList(newList)
+    }
+
+
+}
