299d38bd303e8959dd0ab2c4acb622dfef1afe1f
Zeyad Abdullah <zeyadgalal93@gmail.com>
2023-08-30 14:32:44 +0300

    update cuisine cards

--- app/src/main/java/com/example/masala_food_recipes/ui/recyclerview/CuisineAdapter.kt
+++ app/src/main/java/com/example/masala_food_recipes/ui/recyclerview/CuisineAdapter.kt
@@ -10,31 +10,32 @@ import com.example.masala_food_recipes.R
 import com.example.masala_food_recipes.data.util.Constants
 import com.example.masala_food_recipes.data.util.PreferencesUtil
 import com.example.masala_food_recipes.databinding.CardViewCuisinesBinding
+import com.example.masala_food_recipes.databinding.CuisineCardBinding
 
 
 class CuisineAdapter(items : List<List<String>>) :
     BaseRecyclerAdapter<List<String> , BaseRecyclerAdapter.BaseViewHolder<List<String>>>(
             items) {
-    override val layoutId = R.layout.card_view_cuisines
+    override val layoutId = R.layout.cuisine_card
 
     override fun createViewHolder(view : View) : BaseViewHolder<List<String>> =
             CuisineViewHolder(view)
 
     class CuisineViewHolder(itemView : View) : BaseViewHolder<List<String>>(itemView) {
-        private val binding = CardViewCuisinesBinding.bind(itemView)
+        private val binding = CuisineCardBinding.bind(itemView)
         private val context : Context = itemView.context
 
         @SuppressLint("SetTextI18n")
         override fun bind(item : List<String>) {
             binding.apply {
-                cuisineTextview.text = item[0]
-                itemsCount.text = "${item[1]} item"
+                cuisineName.text = item[0]
+                itemNumber.text = "${item[1]} item"
 
 
                 Glide.with(context).load(item[2]).centerCrop().placeholder(R.drawable.loading)
-                        .into(foodImage)
+                        .into(cuisineImage)
 
-                cardViewCuisine.setOnClickListener {
+                cuisineCard.setOnClickListener {
                     PreferencesUtil.apply {
                         init(context)
                         putInSharedPref(Constants.CUISINE_KEY , item[0])
